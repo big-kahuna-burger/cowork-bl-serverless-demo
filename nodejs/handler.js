a432a0ac0ad79db017c88bf735098e88afdba2af
Arandjel Sarenac <arandjel.sarenac@hotmail.co.uk>
2017-03-29 23:39:57 +0200

    favor lambda integration

--- nodejs/handler.js
+++ nodejs/handler.js
@@ -1,11 +1,5 @@
 'use strict'
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from nodejs!'
-    })
-  }
-  callback(null, response)
+  callback(null, { message: 'Hello from nodejs!' })
 }
