31df4957e924632324fc5376a3a47f5eb2fa7507
Arandjel Sarenac <arandjel.sarenac@hotmail.co.uk>
2017-03-30 13:46:57 +0200

    clarify testing approach

--- nodejs/test/integration.js
+++ nodejs/test/integration.js
@@ -10,7 +10,16 @@ var devApi = 'https://lrh2xoziwj.execute-api.us-east-1.amazonaws.com/dev/'
 test('should call /hello', function (t) {
   t.plan(2)
   hyperquest(devApi + 'hello')
+    .on('response', function (response) {
+      t.equal(response.statusCode, 200, 'expecting corect status code')
+    })
     .pipe(concat(function (data) {
-      t.deepEqual(JSON.parse(data.toString()), {message: 'Hello from nodejs!'}, 'responses should match')
+      t.deepEqual(JSON.parse(data.toString()), { message: 'Hello from nodejs!' }, 'expecting responses to match')
     }))
+    .on('error', function (err) {
+      t.ifError(err, 'should not error')
+    })
+    .on('finish', function () {
+      t.end()
+    })
 })
